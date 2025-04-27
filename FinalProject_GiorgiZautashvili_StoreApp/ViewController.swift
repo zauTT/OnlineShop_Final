9f33d128a1c6b3f3426ece41dbf9af107ee5c021
zauTT <giozau1@gmail.com>
2025-04-27 17:31:02 +0400

    Initial Commit

---
+++ FinalProject_GiorgiZautashvili_StoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalProject_GiorgiZautashvili_StoreApp
+//
+//  Created by Giorgi Zautashvili on 27.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FinalProject_GiorgiZautashvili_StoreAppTests/FinalProject_GiorgiZautashvili_StoreAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProject_GiorgiZautashvili_StoreAppTests.swift
+//  FinalProject_GiorgiZautashvili_StoreAppTests
+//
+//  Created by Giorgi Zautashvili on 27.04.25.
+//
+
+import Testing
+@testable import FinalProject_GiorgiZautashvili_StoreApp
+
+struct FinalProject_GiorgiZautashvili_StoreAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
